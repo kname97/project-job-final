8e932f3ee5fe23a19f4f9d3985553452e6b13ebd
kname97 <quockhanhle1997@gmail.com>
2020-04-26 23:11:48 +0700

    updated 11:11 PM

---
+++ resources/views/form/imageProfile.blade.php
@@ -0,0 +1,8 @@
+<div id="change-image-proifile">
+    <div class="avatar-img">
+        <img src="" alt="">
+    </div>
+    <div class="background-img">
+        <img src="" alt="">
+    </div>
+</div>
